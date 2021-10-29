f7aeea2a4c6ac02afc74fbf1f745c3ba7af3ae59
John Puddephatt <john@jdp.org.uk>
2021-10-30 00:57:13 +0100

    double border

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -5,7 +5,7 @@ Template Name: Home Template
 @extends('layouts.app')
 
 @section('content')
-<div class="flex flex-col h-screen+1">
+<div class="flex flex-col h-screen+1 border-t-8">
   <section class="relative flex-1 overflow-hidden border-t-8" data-simple-slider id="hero-image-slider">
     @foreach($slides as $slide)
     {!! get_the_post_thumbnail($slide->ID, 'large', ['class' => "inset-0 absolute h-full w-full object-cover
