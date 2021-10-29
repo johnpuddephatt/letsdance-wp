d6ee2f8d0add184faaf23f6158ffa060e5e7515d
John Puddephatt <john@jdp.org.uk>
2021-10-30 00:53:36 +0100

    border top

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -6,7 +6,7 @@ Template Name: Home Template
 
 @section('content')
 <div class="flex flex-col h-screen+1">
-  <section class="relative flex-1 overflow-hidden" data-simple-slider id="hero-image-slider">
+  <section class="relative flex-1 overflow-hidden border-t-8" data-simple-slider id="hero-image-slider">
     @foreach($slides as $slide)
     {!! get_the_post_thumbnail($slide->ID, 'large', ['class' => "inset-0 absolute h-full w-full object-cover
     object-center"]) !!}
