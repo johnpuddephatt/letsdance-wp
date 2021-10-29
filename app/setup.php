f3c83bf49a4b675423e81a52e30b83f293aae1da
John Puddephatt <john@jdp.org.uk>
2021-10-29 21:24:23 +0100

    new 4by3 image size

--- app/setup.php
+++ app/setup.php
@@ -60,7 +60,7 @@ add_editor_style('public/styles/editor.css');
  */
 add_action('after_setup_theme', function ()
 {
-
+    add_image_size('16by9-xxxl', 3000, 1688, true);
     add_image_size('16by9-xxl', 2000, 1125, true);
     add_image_size('16by9-xl', 1600, 900, true);
     add_image_size('16by9-l', 1200, 675, true);
@@ -68,6 +68,7 @@ add_action('after_setup_theme', function ()
     add_image_size('16by9-s', 400, 225, true);
     add_image_size('16by9-xs', 240, 135, true);
 
+    add_image_size('2by1-xxxl', 3000, 1500, true);
     add_image_size('2by1-xxl', 2000, 1000, true);
     add_image_size('2by1-xl', 1600, 800, true);
     add_image_size('2by1-l', 1200, 600, true);
@@ -75,6 +76,7 @@ add_action('after_setup_theme', function ()
     add_image_size('2by1-s', 480, 240, true);
     add_image_size('2by1-xs', 240, 120, true);
 
+    add_image_size('3by1-2xl', 3000, 1000, true);
     add_image_size('3by1-xl', 1620, 540, true);
     add_image_size('3by1-l', 1200, 400, true);
     add_image_size('3by1', 810, 270, true);
@@ -87,6 +89,15 @@ add_action('after_setup_theme', function ()
     // add_image_size( 'twothirds', 640, 420, true );
     // add_image_size( 'twothirds-s', 450, 300, true );
     // add_image_size( 'twothirds-xs', 240, 160, true );
+    
+    add_image_size('4by3-xxxl', 3000, 2250, true);
+    add_image_size('4by3-xxl', 1600, 1200, true);
+    add_image_size('4by3-xl', 1200, 900, true);
+    add_image_size('4by3-l', 960, 720, true);
+    add_image_size('4by3', 800, 600, true);
+    add_image_size('4by3-s', 640, 480, true);
+    add_image_size('4by3-xs', 320, 240, true);
+
     add_image_size('square-xxl', 1600, 1600, true);
     add_image_size('square-xl', 1200, 1200, true);
     add_image_size('square-l', 960, 960, true);
