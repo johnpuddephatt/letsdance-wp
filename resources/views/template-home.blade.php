3583a1561942f731ca550121d61e5093cc890e3c
John Puddephatt <john@jdp.org.uk>
2021-10-30 01:08:58 +0100

    fill available

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -5,7 +5,7 @@ Template Name: Home Template
 @extends('layouts.app')
 
 @section('content')
-<div class="flex flex-col h-screen">
+<div class="flex flex-col h-full">
   <section class="relative flex-1 overflow-hidden" data-simple-slider id="hero-image-slider">
     @foreach($slides as $slide)
     {!! get_the_post_thumbnail($slide->ID, 'large', ['class' => "inset-0 absolute h-full w-full object-cover
