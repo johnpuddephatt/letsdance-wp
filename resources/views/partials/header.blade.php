4cb5ab73028d0883290cc5bed3bff52473615d21
John Puddephatt <john@jdp.org.uk>
2021-11-01 22:59:10 +0000

    remove bg from nav on portrait home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header
-  class="z-40 transition-transform delay-75 transform bg-white border-t border-gray-50  @if(isset($is_home) && $is_home) portrait:bg-black portrait:bg-opacity-50 portrait:border-t-0 portrait:absolute left-0 right-0 bottom-0 portrait:bg-transparent portrait:border-t-none  portrait:text-white @endif"
+  class="z-40 transition-transform delay-75 transform bg-white border-t border-gray-50  @if(isset($is_home) && $is_home) portrait:bg-black portrait:bg-opacity-50 portrait:border-t-0 portrait:absolute left-0 right-0 bottom-0 portrait:border-t-none  portrait:text-white @endif"
   :class="{'translate-y-full transition-none' : $store.siteLoading}" role="banner">
   <div class="container flex items-center justify-between h-12 lg:h-20 max-w-none">
     <div>
