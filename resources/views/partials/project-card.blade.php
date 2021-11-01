15afcc4b5e3e95daba8b5d82b5a7f76fd8c876b3
John Puddephatt <john@jdp.org.uk>
2021-11-01 14:30:26 +0000

    leading on project card excerpt

--- resources/views/partials/project-card.blade.php
+++ resources/views/partials/project-card.blade.php
@@ -3,12 +3,11 @@
     <div class="transition-transform transform group-hover:scale-101">
         {!! get_the_post_thumbnail($project->ID, '2by1', ['class' => "block rounded border border-gray-50"]) !!}
     </div>
-    <h3
-        class="mt-4 mb-1.5 text-base antialiased font-semibold leading-tight lg:leading-none text-gray-700 lg:mb-2 lg:text-lg">
+    <h3 class="mt-4 mb-1.5 text-base antialiased font-semibold leading-none text-gray-700 lg:mb-2 lg:text-lg">
         {!!
         $project->post_title !!}
     </h3>
-    <p class="font-serif text-2xl antialiased leading-none text-gray-800 lg:text-3xl">{!!
+    <p class="font-serif text-2xl antialiased leading-tight text-gray-800 lg:leading-none lg:text-3xl">{!!
         $project->post_excerpt
         !!}
     </p>
