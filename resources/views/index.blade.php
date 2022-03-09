99dbd879c98e8ace8f07c1d602cd17a678d82f4f
John Puddephatt <john@jdp.org.uk>
2022-03-10 00:13:44 +0530

    debug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app') @section('content') @include('partials.header')
 
 <div
-  class="container my-16 space-y-16 lg:my-24 lg:space-y-24 xl:max-w-7xl 2xl:my-64"
+  class="container relative my-16 space-y-16 lg:my-24 lg:space-y-24 xl:max-w-7xl 2xl:my-64"
 >
   @if(! have_posts())
 
