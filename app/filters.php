23a137cbb940b19fb3ce3e184edabb51b3b067a8
John Puddephatt <john@jdp.org.uk>
2022-06-17 00:42:23 +0100

    update image removing filter

--- app/filters.php
+++ app/filters.php
@@ -11,20 +11,18 @@ namespace App;
  *
  * @return string
  */
-add_filter('excerpt_more', function () {
+add_filter("excerpt_more", function () {
     return null;
     // return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
 
-remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+remove_filter("get_the_excerpt", "wp_trim_excerpt");
 
-
-
-add_filter('body_class', function($classes) {
+add_filter("body_class", function ($classes) {
     return array_merge($classes, [
-        'min-h-screen',
-        'no-focus-outline',
-        'overflow-x-hidden',
+        "min-h-screen",
+        "no-focus-outline",
+        "overflow-x-hidden",
     ]);
 });
 
@@ -32,19 +30,34 @@ add_filter('body_class', function($classes) {
 //     return 'foo';
 // }, 10, 6 );
 
+add_filter(
+    "the_content",
+    function ($content) {
+        if (is_singular() && in_the_loop() && is_main_query()) {
+            $content = preg_replace(
+                "<!-- wp:post-featured-image(.*?)-->",
+                "",
+                $content
+            );
 
-add_filter( 'the_content', function ( $content ) {
-    if ( is_singular() && in_the_loop() && is_main_query() ) {
-        $content = str_replace('<!-- wp:post-featured-image {"align":"wide","className":"project-template\u002d\u002dfeatured-image","lock":{"remove":true,"move":true}} /-->', '', $content);
-        $content = str_replace('<!-- wp:post-featured-image {"align":"wide","className":"post-template\u002d\u002dfeatured-image","lock":{"remove":true,"move":true}} /-->', '', $content);
-    }
-    return $content;
-}, 1);
+            $content = str_replace("<>", "", $content);
+        }
+        return $content;
+    },
+    1
+);
 
-
-add_filter( 'the_content', function ( $content ) {
-    if ( is_singular() && in_the_loop() && is_main_query() ) {
-        return preg_replace('#https://player\.vimeo\.com/video/(\d+)\?#i', 'https://player.vimeo.com/video/$1?color=bdece1&title=0&byline=0&portrait=0&', $content);
-    }
-    return $content;
-}, 99);
+add_filter(
+    "the_content",
+    function ($content) {
+        if (is_singular() && in_the_loop() && is_main_query()) {
+            return preg_replace(
+                "#https://player\.vimeo\.com/video/(\d+)\?#i",
+                'https://player.vimeo.com/video/$1?color=bdece1&title=0&byline=0&portrait=0&',
+                $content
+            );
+        }
+        return $content;
+    },
+    99
+);
