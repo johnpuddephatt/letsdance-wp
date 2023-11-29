6c317085a1bd080f5bece7cd3612958b2003515c
John Puddephatt <john@jdp.org.uk>
2023-11-29 21:47:38 +0000

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,13 @@
     {{ __('Skip to content') }}
   </a>
 
-  @if(is_page_template('template-home.blade.php'))
-  @include('partials.loading')
-  @endif
+@if(is_page_template('template-home.blade.php'))
+hi
+@include('partials.loading')
+@endif
 
   <main id="main" class="bg-white main" tabindex="-1" data-barba="container"
     data-barba-namespace="{{ get_page_template_slug() ? str_replace('.blade.php','',str_replace('template-','', get_page_template_slug())) : ($type ?? 'standard') }}">
     @yield('content')
   </main>
-</div>
-
 </div>
