68843069438ba913628ae8d367e2d11b06c7466e
John Puddephatt <john@jdp.org.uk>
2023-11-29 21:30:10 +0000

    include blocks css

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="/wp-includes/css/dist/block-library/style.min.css" />
     <?php wp_head(); ?>
   </head>
 
