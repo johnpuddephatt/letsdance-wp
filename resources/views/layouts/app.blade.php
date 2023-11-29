17daab15e061842bccb33366ca20e239db31c06e
John Puddephatt <john@jdp.org.uk>
2023-11-29 21:55:04 +0000

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,6 @@
 <div x-data role="document" data-barba="wrapper">
 
-  <a class="sr-only focus:not-sr-only" href="#main">
-    {{ __('Skip to content') }}
-  </a>
 
-@if(is_page_template('template-home.blade.php'))
-hi
-@include('partials.loading')
-@endif
 
   <main id="main" class="bg-white main" tabindex="-1" data-barba="container"
     data-barba-namespace="{{ get_page_template_slug() ? str_replace('.blade.php','',str_replace('template-','', get_page_template_slug())) : ($type ?? 'standard') }}">
