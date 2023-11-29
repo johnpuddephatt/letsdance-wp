9b4cd40379ed0d27157043e644b59c2583deab93
John Puddephatt <john@jdp.org.uk>
2023-11-29 21:57:27 +0000

    try

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,11 @@
 @include('partials.loading')
 @endif
 
+
+<a class="sr-only focus:not-sr-only" href="#main">
+    {{ __('Skip to content') }}
+  </a>
+
   <main id="main" class="bg-white main" tabindex="-1" data-barba="container"
     data-barba-namespace="{{ get_page_template_slug() ? str_replace('.blade.php','',str_replace('template-','', get_page_template_slug())) : ($type ?? 'standard') }}">
     @yield('content')
