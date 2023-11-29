0fb9c543a863b3f9e764b3897cd1c6f7a8b00b1a
John Puddephatt <john@jdp.org.uk>
2023-11-29 21:55:32 +0000

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <div x-data role="document" data-barba="wrapper">
 
 
+@if(is_page_template('template-home.blade.php'))
+@include('partials.loading')
+@endif
 
   <main id="main" class="bg-white main" tabindex="-1" data-barba="container"
     data-barba-namespace="{{ get_page_template_slug() ? str_replace('.blade.php','',str_replace('template-','', get_page_template_slug())) : ($type ?? 'standard') }}">
